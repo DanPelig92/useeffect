f8c36ee9d0cf3c29f043de16bf04e5163f83de58
Dan <you@example.com>
2022-11-24 12:26:34 +0100

    useEffect integrato

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import Button from "./Button"
+import {useState} from "react"
+import {useEffect} from "react"
+
 function App() {
+
+
+
+const[content, setContent] = useState("");
+useEffect(()=>{console.log("stringa")},[content])
+const str = ['Utenti', 'Tweet', 'Commenti']
+
+function show(stringhe){
+  setContent(stringhe)
+  console.log('aggiornato')
+}
+
+console.log('Componente caricata...')
+
   return (
     <div>
-      Hello world!
+      <Button azione={() =>show(str[0])} nome="Utenti"/>
+      <Button azione={() =>show(str[1])} nome="Tweet"/>
+      <Button azione={() =>show(str[2])} nome="Commenti"/>
+      <div>{content}</div>
     </div>
   );
 }
